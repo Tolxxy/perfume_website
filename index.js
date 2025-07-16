b5cec16e9a1eb666a34a9bb2c871f35fa1075a8c
johnson Nicholas <nicholasjohnson.njt@gmail.com>
2025-07-16 23:06:32 +0100

    made some adjustment

--- index.js
+++ index.js
@@ -1,6 +1,36 @@
 const navLinks = document.querySelector('.nav-links');
 function onToggleMenu(icon){
-    icon.name = icon.name === 'menu' ? 'close' : 'menu';
-    navLinks.classList.toggle('top-[9%]');
+    icon.name = icon.name === 'close' ? 'menu' : 'close';
+    navLinks.classList.toggle('top-[100%]');
 }
 
+// add to cart functionality
+document.addEventListener('DOMContentLoaded', () => {
+  const cartCount = document.getElementById('cart-count');
+  let count = 0;
+
+  const addToCartButtons = document.querySelectorAll('.add-to-cart');
+
+  addToCartButtons.forEach(button => {
+    button.addEventListener('click', () => {
+      count += 1;
+      cartCount.textContent = count;
+    });
+  });
+});
+
+
+// search drop dwon
+const searchIcon = document.getElementById('search-icon');
+const searchInput = document.getElementById('search');
+const cancel = document.getElementById('cancel');
+searchIcon.addEventListener('click', () => {
+  searchInput.classList.add('top-0');
+});
+
+//
+    cancel.addEventListener('click', () => {
+        searchInput.classList.remove('top-0');
+       
+    });
+// 
